82aa8c4fcdd1ca20e0113f2922142a2627a7d56b
Vinicius Moura <vinijmoura@hotmail.com>
2022-02-16 18:21:26 -0300

    Create report to show all Repositories and branches to identify the gap (ahead/behind) between each branch and default branch within organization

---
+++ PowerShell/RepositoriesBranchesAheadBehind/RepositoriesBranchesAheadBehind.ps1
@@ -0,0 +1,43 @@
+﻿Param
+(
+    [string]$PAT,
+    [string]$Organization,
+    [string]$Connstr
+)
+
+$SQLQuery = "TRUNCATE TABLE RepositoriesBranchesAheadBehind"
+Invoke-Sqlcmd -query $SQLQuery -ConnectionString $Connstr
+
+$UriOrganization = "https://api.github.com/orgs/$($organization)"
+$UriRepos = "https://api.github.com/repos/$($organization)"
+
+$base64Token = [System.Convert]::ToBase64String([char[]]$PAT)
+$headers = @{Authorization = 'Basic {0}' -f $base64Token};
+
+$uriRepositories = "$($UriOrganization)/repos"
+$RepositoriesResult = Invoke-RestMethod -Headers $headers -Uri $uriRepositories
+foreach ($repo in $RepositoriesResult)
+{
+    $urlBranchesRepo = $repo.branches_url.Replace('{/branch}','')
+    $BranchesRepoResult = Invoke-RestMethod -Headers $headers -Uri $urlBranchesRepo
+    foreach ($branchRepo in $BranchesRepoResult)
+    {
+        $uriCompare = "$($UriRepos)/$($repo.name)/compare/$($repo.default_branch)...$($branchRepo.name)"
+        $ComparesResult = Invoke-RestMethod -Headers $headers -Uri $uriCompare
+        $SQLQuery = "INSERT INTO RepositoriesBranchesAheadBehind (
+                                RepositoryId,
+                                RepositoryName,
+                                RepositoryBranchName,
+                                RepositoryBranchAheadCount,
+                                RepositoryBranchBehindCount
+                                )
+                                VALUES(
+                                '$($repo.id)',
+                                '$($repo.name)',
+                                '$($branchRepo.name)',
+                                $($ComparesResult.ahead_by),
+                                $($ComparesResult.behind_by)
+                                )"
+        Invoke-Sqlcmd -query $SQLQuery -ConnectionString $Connstr
+    }
+}
---
+++ PowerShell/RepositoriesBranchesAheadBehind/RepositoriesBranchesAheadBehind.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[RepositoriesBranchesAheadBehind](
+	RepositoryId						[varchar](20) NOT NULL,
+	RepositoryName						[varchar](100) NOT NULL,
+	RepositoryBranchName				[varchar](50) NOT NULL,
+	RepositoryBranchAheadCount			[int] NULL,
+	RepositoryBranchBehindCount			[int] NULL,
+)
