94175e96c6e6c96cdbc526912ec241b637089b15
Vinicius Moura <vinijmoura@hotmail.com>
2022-02-16 19:52:37 -0300

    Update RepositoriesBranchesAheadBehind.sql

--- PowerShell/RepositoriesBranchesAheadBehind/RepositoriesBranchesAheadBehind.sql
+++ PowerShell/RepositoriesBranchesAheadBehind/RepositoriesBranchesAheadBehind.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[RepositoriesBranchesAheadBehind](
-	RepositoryId						[varchar](20) NOT NULL,
-	RepositoryName						[varchar](100) NOT NULL,
+	RepositoryId					[varchar](20) NOT NULL,
+	RepositoryName					[varchar](100) NOT NULL,
 	RepositoryBranchName				[varchar](50) NOT NULL,
 	RepositoryBranchAheadCount			[int] NULL,
 	RepositoryBranchBehindCount			[int] NULL,
