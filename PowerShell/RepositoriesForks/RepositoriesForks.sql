05ca0f248b8408fcc34a6ba1b732c16043bcbe3a
Vinicius Moura <vinijmoura@hotmail.com>
2022-09-02 09:28:49 -0300

    Update RepositoriesForks.sql

--- PowerShell/RepositoriesForks/RepositoriesForks.sql
+++ PowerShell/RepositoriesForks/RepositoriesForks.sql
@@ -1,5 +1,5 @@
 CREATE TABLE [dbo].[RepositoriesForks](
 	RepositoryName						[varchar](100) NOT NULL,
-	ForkBy								[varchar](100) NOT NULL,
+	ForkBy						        [varchar](100) NOT NULL,
 	ForkCreatedDate						[datetime] NOT NULL
 )
