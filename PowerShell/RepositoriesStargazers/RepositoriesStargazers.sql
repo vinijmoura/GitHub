e117a82e3ff7acc2d12df1fc6f1cf6b2afb3af11
Vinicius Moura <vinijmoura@hotmail.com>
2022-09-04 22:01:36 -0300

    Update RepositoriesStargazers.sql

--- PowerShell/RepositoriesStargazers/RepositoriesStargazers.sql
+++ PowerShell/RepositoriesStargazers/RepositoriesStargazers.sql
@@ -2,5 +2,5 @@ CREATE TABLE [dbo].[RepositoriesStargazers](
 	RepositoryName			[varchar](100) NOT NULL,
 	StargazerLogin			[varchar](100) NOT NULL,
 	StargazerAvatarUrl		[nvarchar](MAX) NOT NULL,
-	StargazerCreatedDate	[datetime] NOT NULL
+	StargazerCreatedDate		[datetime] NOT NULL
 )
